a38c7a4cc003f436493252a5922ae9becf821077
doczipeter <doczipeter@fedora>
2019-10-27 19:55:17 +0100

    inprog

---
+++ inprog/precache-manifest.7db0a2d6a9019fabace361decf11dd20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e774aef7194249d850da3d9be86b1e5f",
+    "url": "/alternativa/index.html"
+  },
+  {
+    "revision": "85367c573851b7441385",
+    "url": "/alternativa/static/css/main.7b217b90.chunk.css"
+  },
+  {
+    "revision": "e6b00d8454ac85b39e8d",
+    "url": "/alternativa/static/js/2.6f39308b.chunk.js"
+  },
+  {
+    "revision": "85367c573851b7441385",
+    "url": "/alternativa/static/js/main.83f389b3.chunk.js"
+  },
+  {
+    "revision": "90c7877667b9997c3861",
+    "url": "/alternativa/static/js/runtime~main.29e15e83.js"
+  }
+]);
